44b793664a160ce486e4de828a6a5de37e8b557d
Théo Mafaro <theomafaro@gmail.com>
2023-03-02 11:55:28 +0100

    Ajout d'une base pour le front

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.get("/", function(req, res) {
 });
 
 app.listen(8080, function() {
-  console.log("Example app listening on port 8080!");
+  console.log("Server is working on port : 8080");
 });
